580574c5627d7b49964672b7af65d00a4df17483
azeez1776 <aboud3am81@gmail.com>
2021-12-27 17:39:23 +0300

    added options

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ let model;
 
 function setup() {
     createCanve(400, 400);
+
+    let options = {
+        input: ['x', 'y'],
+        output: ['label'],
+        task: 'classification',
+        debug: 'true'
+    }
     background(255);
 }
